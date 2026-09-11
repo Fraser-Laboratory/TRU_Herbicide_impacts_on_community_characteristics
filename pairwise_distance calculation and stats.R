736b51025c12b0a3879222a554b7ff418fec591a
Jay Prakash Singh <jsingh@tru.ca>
2026-09-11 13:17:38 -0700

    Tentative code for pairwise distance calculations and stats. This will eventually be included in mastercode

---
+++ pairwise_distance calculation and stats.R
@@ -0,0 +1,40 @@
+library(rstatix)
+
+
+
+
+##calculating pairwise dissimilarity between baseline and rest of the samples across timepoints
+## we can use the same code to calculate pairwise distance for turnover, nestedness and unifrac
+## that gives a more compelling effidence that betadiversity and partitioning is impacted by herbicide application
+dist_from_baseline <- bray_dist %>% 
+ pull_lower_triangle() %>% 
+  as.data.frame() %>% 
+  pivot_longer(-rowname) %>% 
+  filter(value != "") %>% 
+  mutate(name_time = factor(name, levels = meta$id, labels = meta$year),
+         rowname_time = factor(rowname, levels = meta$id, labels = meta$year),
+         sprayed = factor(rowname, levels = meta$id, labels = meta$sprayed),
+         value = as.numeric(value)) %>% 
+  filter(rowname_time != "2018",
+         name_time  == "2018") %>% 
+  group_by(name,rowname_time,sprayed ) %>% 
+  summarise(mean_val = mean(value)) %>% ungroup()
+
+str(dist_from_baseline)
+
+
+m_braydist <- lmer((mean_val) ~ rowname_time * sprayed + (1|name), 
+                   dist_from_baseline) 
+DHARMa::simulateResiduals(m_braydist, plot = T)
+car::Anova(m_braydist, test.statistic = "F")
+  ggplot(dist_from_baseline, aes(x = sprayed, y = mean_val)) +
+  stat_summary(geom = "pointrange", fun.data = "mean_sdl", linewidth = 1.5, size = 1.5, color = "steelblue") +
+  facet_grid(~ rowname_time) +
+    ylim(c(0, NA)) +
+    ggpubr::geom_pwc(label = "p.adj.signif") +
+    theme_bw() +
+    theme(text = element_text(color = "black", face = "bold", size = 14))+
+    labs(x = "Sprayed",
+         y = "Mean Bray-Curtis dissimilarity from baseline") 
+  
+  
