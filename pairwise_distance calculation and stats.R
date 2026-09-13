160373a651e80803175d4e3cfdcb8da1c8ba975c
Jay Prakash Singh <jsingh@tru.ca>
2026-09-13 13:33:23 -0700

    Created as a function

--- pairwise_distance calculation and stats.R
+++ pairwise_distance calculation and stats.R
@@ -6,7 +6,8 @@ library(rstatix)
 ##calculating pairwise dissimilarity between baseline and rest of the samples across timepoints
 ## we can use the same code to calculate pairwise distance for turnover, nestedness and unifrac
 ## that gives a more compelling effidence that betadiversity and partitioning is impacted by herbicide application
-dist_from_baseline <- bray_dist %>% 
+dist_analysis <- function(dat_mat){
+dist_from_baseline <- dat_mat %>% 
  pull_lower_triangle() %>% 
   as.data.frame() %>% 
   pivot_longer(-rowname) %>% 
@@ -23,18 +24,32 @@ dist_from_baseline <- bray_dist %>%
 str(dist_from_baseline)
 
 
-m_braydist <- lmer((mean_val) ~ rowname_time * sprayed + (1|name), 
+m_dist <- lmer((mean_val) ~ rowname_time * sprayed + (1|name), 
                    dist_from_baseline) 
-DHARMa::simulateResiduals(m_braydist, plot = T)
-car::Anova(m_braydist, test.statistic = "F")
-  ggplot(dist_from_baseline, aes(x = sprayed, y = mean_val)) +
-  stat_summary(geom = "pointrange", fun.data = "mean_sdl", linewidth = 1.5, size = 1.5, color = "steelblue") +
+DHARMa::simulateResiduals(m_dist, plot = T)
+car::Anova(m_dist, test.statistic = "F")
+  plt <- ggplot(dist_from_baseline, aes(x = sprayed, y = mean_val)) +
+  # stat_summary(geom = "pointrange", fun.data = "mean_sdl",
+  #              linewidth = 1.5, size = 1.5, color = "black",
+  #              shape = 23, fill = "steelblue", fun.args = list(mult = 1)) +
+  geom_boxplot()+
   facet_grid(~ rowname_time) +
     ylim(c(0, NA)) +
     ggpubr::geom_pwc(label = "p.adj.signif") +
+    scale_x_discrete(labels = c("FALSE" = "Unsprayed",
+                                "TRUE" = "Sprayed")) +
     theme_bw() +
-    theme(text = element_text(color = "black", face = "bold", size = 14))+
-    labs(x = "Sprayed",
-         y = "Mean Bray-Curtis dissimilarity from baseline") 
+    theme(text = element_text(color = "black", face = "bold", size = 14),
+          axis.text = element_textbox(color = "black", face = "bold", size = 14)) #+
+    # labs(x = NULL,
+    #      y = "Mean Bray-Curtis dissimilarity from baseline")
+    # 
+    # 
   
-  
+  return(plt)
+}
+dist_analysis(bray_dist)
+dist_analysis(weighted_unifrac)
+dist_analysis(soren$beta.sor)
+dist_analysis(soren$beta.sne)
+
