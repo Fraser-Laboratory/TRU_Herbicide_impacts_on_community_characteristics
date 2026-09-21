16c3d2bb06df7e4d35b22b62c87800774477caf0
Matthew Coghill <matthewcoghill@live.ca>
2026-09-21 09:14:34 -0700

    Revision update

--- Master_Code_Publication.R
+++ Master_Code_Publication.R
@@ -438,7 +438,7 @@ weighted_unifrac <- unifracs[, , "d_0.5"]
 set.seed(213)
 phy_ado2 <- adonis2(weighted_unifrac ~ sprayed * ash * time, by = "term", data = meta)
 phy_ado2_df <- data.frame(phy_ado2)
-write.csv(phy_ado2_df, "Output/Weighed_Unifrac_PERMANOVA.csv", row.names = TRUE)
+write.csv(phy_ado2_df, "Output/Weighted_Unifrac_PERMANOVA.csv", row.names = TRUE)
 
 # NMDS for weighted unifrac distances
 set.seed(2112)
@@ -487,7 +487,7 @@ phy_nmds <- ggplot(phy_plt1, aes(x = NMDS1, y = NMDS2, fill = sprayed)) +
     axis.text = element_text(color = "black"),
     panel.grid = element_blank()
   ) +
-  labs(title = "NMDS of weighed Unifrac distance")
+  labs(title = "NMDS of weighted Unifrac distance")
 phy_nmds
 
 # Combine plots
